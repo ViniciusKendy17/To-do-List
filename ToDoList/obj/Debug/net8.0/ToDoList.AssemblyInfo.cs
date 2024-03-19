dd711f3986cdff936d6a4c40489beadaccdeadc3
ViniciusKendy17 <135672206+ViniciusKendy17@users.noreply.github.com>
2024-03-19 13:21:22 -0300

    aaa
    aaa

--- ToDoList/obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
+++ ToDoList/obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59b2fe45e11937d7817014509441225e988a0ebf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+241966bb9c000124ed77c043a3f0e517b0904b2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
